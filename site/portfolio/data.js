f0d4b72d064b058698e5dd3412cd9c7fa2195f7f
Clay Diffrient <cdiffrient@instructure.com>
2016-11-13 23:05:38 -0700

    Add portfolio with fake data

---
+++ site/portfolio/data.js
@@ -0,0 +1,32 @@
+const fakeData = [];
+
+for (let i = 0; i < 30; i++) {
+  fakeData.push({
+    imageUrl: 'http://placehold.it/350x150',
+    title: 'Blog template',
+    text: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
+  });
+}
+
+
+const PORTFOLIO_DATA = [{
+  imageUrl: 'http://placehold.it/350x150',
+  title: 'Blog template',
+  text: 'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.',
+}];
+
+export default fakeData; // PORTFOLIO_DATA;
+
+
+// {/* <div className="mdl-card__media">
+//   <img className="article-image" src="" alt="" />
+// </div>
+// <div className="mdl-card__title">
+//   <h2 className="mdl-card__title-text">Blog template</h2>
+// </div>
+// <div className="mdl-card__supporting-text">
+//               Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
+//           </div>
+// <div className="mdl-card__actions mdl-card--border">
+//   <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent" href="portfolio-example01.html">Read more</a>
+// </div> */}
--- site/portfolio/index.js
+++ site/portfolio/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import PROJECTS from './data';
+import styles from './portfolio.css';
 
 const Portfolio = () => (
-  <div>Testing</div>
+  <div className={`mdl-grid ${styles.root}`}>
+    {
+      PROJECTS.map(p => (
+        <div className="mdl-cell mdl-card mdl-shadow--4dp">
+          <div className="mdl-card__media">
+            <img className={styles.image} src={p.imageUrl} alt="" />
+          </div>
+          <div className={`mdl-card__title ${styles.cardTitle}`}>
+            <h2 className="mdl-card__title-text">{p.title}</h2>
+          </div>
+          <div className="mdl-card__supporting-text">{p.text}</div>
+          <div className="mdl-card__actions mdl-card--border">
+            <a
+              className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-button--accent"
+              href="portfolio-example01.html"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      ))
+    }
+  </div>
 );
 
 export default Portfolio;
